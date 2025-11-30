f7e5ebb1a62cf384b86d07f84549c5a90da3ea2c
jaime asensio <jimy.asensio@gmail.com>
2025-11-30 16:44:33 +0100

    front crm jaime

---
+++ src/app/modules/clienten/clienten.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientenRoutingModule } from './clienten-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientenRoutingModule
+  ]
+})
+export class ClientenModule { }
